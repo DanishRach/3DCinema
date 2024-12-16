65732ed856a989e34d43ab252ce7b03f2fa23137
Danish As-Salafy Rachman <129130056+DanishRach@users.noreply.github.com>
2024-12-16 12:35:55 +0700

    another commit

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Specify the output directory as "build"
+    outDir: 'build', // Specify the output directory as "build"
   },
 })
